0bb966056652f9908c2a38afd31c5216cb454584
BrackListed <cameta8900@gmail.com>
2026-05-20 17:02:30 +0800

    Added the header UI for the main dashboard. Displays the callsign of the squad member when you click on them

--- src/components/MainMonitor.tsx
+++ src/components/MainMonitor.tsx
@@ -8,8 +8,9 @@ export function MainMonitor() {
     const selectedId = useSelector((state: RootState) => state.squadronCharacters.selectedId)
     const selectedCharacter = characters.find((c) => c.id === selectedId)
     return(
-        <div id = "main-dashboard thing">
-            <div id = "header">{selectedCharacter?.name}</div>
+        <div id = "main-dashboard thing" className="flex flex-col p-6 h-full gap-5 ">
+            <div className="text-3xl font-bold tracking-wide text-green-500 ">MAIN MONITOR: REGINA SYNC PROFILE </div>
+            <div id = "callsign" className="text-2xl">[ SELECTED PILOT: <span className="text-red-500">{selectedCharacter!.callSign}</span> ]</div>
         </div>
     ) //render different stuff out based on who they are honestly idk how to go about this
 }
