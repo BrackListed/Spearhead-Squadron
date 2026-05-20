00d3002280d658960eafc2c93ac7ab7e0c5d22d7
BrackListed <cameta8900@gmail.com>
2026-05-20 16:55:02 +0800

    Displays name whenever you click a character now

--- src/components/MainMonitor.tsx
+++ src/components/MainMonitor.tsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux"
+import type { RootState } from "../Store"
+import { selectCharacter } from "../SquadronSlice"
 
 
 export function MainMonitor() {
-    return null
+    const characters = useSelector((state: RootState) => state.squadronCharacters.characterList)
+    const selectedId = useSelector((state: RootState) => state.squadronCharacters.selectedId)
+    const selectedCharacter = characters.find((c) => c.id === selectedId)
+    return(
+        <div id = "main-dashboard thing">
+            <div id = "header">{selectedCharacter?.name}</div>
+        </div>
+    ) //render different stuff out based on who they are honestly idk how to go about this
 }
